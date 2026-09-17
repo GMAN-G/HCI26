79a3ad11e8094141f48896c2f52757cdb52890d6
GMAN-G <a47406338@gmail.com>
2026-09-17 14:46:54 +0900

    9.17

---
+++ HCI26/Assets/2.Scripts/2/TankMoveEvent.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.Rendering.DebugUI;
+
+public class TankMoveEvent : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public float rotateSpeed = 2f;
+    float move;
+    float rotate;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Mathf.Abs(move) > 0.1f || Mathf.Abs(rotate) > 0.1f)
+        {
+            Move();
+            Rotate();
+        }
+    }
+    /* void OnMove(InputValue value)
+     {
+         //Debug.Log("Move value : " + value.Get<float>());
+         move = value.Get<float>();
+     }
+
+     void OnRotate(InputValue value)
+     {
+         //Debug.Log("Rotate value : " + value.Get<float>());
+         rotate = value.Get<float>();
+     } */
+
+    public void OnTankMove(InputAction.CallbackContext context)
+    {
+        // Debug.Log("Move");
+        // Debug.Log("Move value : " + context.ReadValue<float>());
+        move = context.ReadValue<float>();
+    }
+
+    public void OnTankRotate(InputAction.CallbackContext context)
+    {
+        // Debug.Log("Move");
+        // Debug.Log("Rotate value : " + context.ReadValue<float>());
+        rotate = context.ReadValue<float>();
+    }
+
+    void Move()
+    {
+        Vector3 moveDir = transform.forward * move * moveSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + moveDir);
+    }
+    void Rotate()
+    {
+        float rotSpeed = rotate * rotateSpeed * Time.deltaTime;
+        rb.rotation = Quaternion.Euler(0, rotSpeed, 0) * rb.rotation;
+    }
+}
